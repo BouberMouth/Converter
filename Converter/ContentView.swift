830cdecdd2a444ebc038455d81a3462b195c33df
Antoine <bababouch07@gmail.com>
2020-07-15 16:10:29 +0200

    - Convert into SwiftUI project

---
+++ Converter/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Converter
+//
+//  Created by Antoine on 15/07/2020.
+//  Copyright © 2020 BOUBERBOUCHE Antoine. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
